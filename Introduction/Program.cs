64bb3ca33aabec623727def09daa0af6cf869ca8
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-10-25 17:17:34 -0400

    Add function to show the five largest files in a directory, formatted and written to console with string interpolation.

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Introduction
 {
@@ -6,7 +9,28 @@ namespace Introduction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Linq Fundamentals - based on Pluralsight class by Scott Allen\n");
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+        }
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            Console.WriteLine("WITHOUT LINQ\n");
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+            Console.WriteLine("\n* * *\n\n");
+        }
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+            {
+                return y.Length.CompareTo(x.Length);
+            }
         }
     }
 }
