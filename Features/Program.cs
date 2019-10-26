8c46218a14464d156ff26176084fcd48e6b36f80
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-10-25 23:07:52 -0400

    Added Features project to the solution.
    Features program.cs is demonstrating IEnumerable
    
    Iterating through with IEnumerator
    
    Added extension method Count in MyLinq.cs

---
+++ Features/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARRAY of developers
+            IEnumerable<Employee> developer = new Employee[]
+            {
+                new Employee { Id = 1, Name="Scott"},
+                new Employee { Id = 2, Name="Chris"}
+            };
+
+
+            // LIST of sales employees
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee {Id = 3, Name = "Alex"}
+            };
+
+            // Iterate through developers array
+
+            IEnumerator<Employee> enumerator = developer.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+            // Count() is an extension method in MyLinq.cs
+            Console.WriteLine("Number of Developers: " + developer.Count() + "\n");
+
+            // Iterate through sales list
+            enumerator = sales.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+            Console.WriteLine("Number of Sales Employees: " + sales.Count() + "\n");
+        }
+    }
+}
