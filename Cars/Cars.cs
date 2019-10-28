48b1287ebeb7e5e1837655b564ca612c27ad193a
Eugenie Amalfitano <eamalfitano@gmail.com>
2019-10-27 21:59:31 -0400

    Parse the cars CSV file, then use Linq to map each line to the Car object and add to a list.
    Then, extract the name of each car and log to console.

--- Cars/Cars.cs
+++ Cars/Cars.cs
@@ -1,4 +1,7 @@
-﻿namespace Cars
+﻿using System;
+using System.Data;
+using System.IO;
+namespace Cars
 {
     public class Car
     {
@@ -10,5 +13,23 @@
         public int City { get; set; }
         public int Highway { get; set; }
         public int Combined { get; set; }
+
+        internal static Car ParseFromCsv(string line)
+        {
+            var columns = line.Split(',');
+
+            return new Car
+            {
+                Year = int.Parse(columns[0]),
+                Manufacturer = columns[1],
+                Name = columns[2],
+                Displacement = double.Parse(columns[3]),
+                Cylinders = int.Parse(columns[4]),
+                City = int.Parse(columns[5]),
+                Highway = int.Parse(columns[6]),
+                Combined = int.Parse(columns[7])
+            };
+        }
+
     }
 }
